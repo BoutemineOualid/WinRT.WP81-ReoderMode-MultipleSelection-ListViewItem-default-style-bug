c8abba2ee9ebed9699dc926172e017804b2655f2
Oualid Boutemine <boutemine.walid@hotmail.com>
2014-08-13 11:22:24 -0400

    initial commit
    A sample WP81 project to reproduce the reordering bug that appears when
    setting ReorderMode to Enabled and SelectionMode to multiple on a
    ListView on which ListViewItem objects are applying the default style as
    described here
    http://msdn.microsoft.com/en-us/library/windows/apps/xaml/jj709921.aspx

---
+++ App1/App1/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Model;
+
+namespace App1.ViewModel
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            LoadSampleData();
+        }
+
+        private void LoadSampleData()
+        {
+            Items = new ObservableCollection<Item>(new[]
+            {
+                new Item{Name = "Item 1", Description = "Item 1 Description"},
+                new Item{Name = "Item 2", Description = "Item 2 Description"},
+                new Item{Name = "Item 3", Description = "Item 3 Description"},
+                new Item{Name = "Item 4", Description = "Item 4 Description"},
+            });
+        }
+
+        public ObservableCollection<Item> Items { get; set; }
+
+    }
+}
